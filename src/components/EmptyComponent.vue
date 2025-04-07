e0a0161d18a1d09f83a4e6a32f6fa775bd508fc9
VictorMRD <victormrd2002@hotmail.com>
2025-04-06 19:40:12 -0700

    Install tailwindcss
    Removed boilerplate code from the vue framework

---
+++ src/components/EmptyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="empty-component">
+        <!-- Your content goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EmptyComponent',
+};
+</script>
+
+<style scoped>
+.empty-component {
+    /* Your styles go here */
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="">
+         <p>Hello</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+</style>
