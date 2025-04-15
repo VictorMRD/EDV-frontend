bb5775e350f668c025825ed420088084b625352b
VictorMRD <victormrd2002@hotmail.com>
2025-04-15 13:29:25 -0700

    Add input component

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/auth/Login.vue'),
+      component: () => import('../views/auth/UserLogin.vue'),
     },
   ],
 })
---
+++ src/views/auth/UserLogin.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center bg-gray-400 w-50">
+        <p>Login</p>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <Button>Login</Button>
+    </div>
+</template>
