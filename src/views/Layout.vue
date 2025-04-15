f284ffb92b4b3a0455576221065665f7491d3083
VictorMRD <victormrd2002@hotmail.com>
2025-04-15 13:18:14 -0700

    Add Layout to project

---
+++ src/views/Layout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import { RouterLink, RouterView } from 'vue-router'
+</script>
+<template>
+    <div>
+        Header
+    </div>
+    <div>
+        <RouterView />
+    </div>
+    <div>
+        Footer
+    </div>
+</template>
