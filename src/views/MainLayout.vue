a9c80f4867d866adae701e08b2872162ac1fbd8b
VictorMRD <victormrd2002@hotmail.com>
2025-04-15 13:57:42 -0700

    Change name of Layout to MainLayout

---
+++ src/views/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import Button from '@/components/ui/button/Button.vue';
+    import { ref } from 'vue';
+    import { RouterLink, RouterView } from 'vue-router'
+    import { useColorMode } from '@vueuse/core'    
+    
+    const mode = useColorMode()
+</script>
+<template>
+    <div>
+        Header
+        <div>
+            Change style
+            <Button>{{ mode ? "Dark" : "Light" }}</Button>
+        </div>
+    </div>
+    <div>
+        <RouterView />
+    </div>
+    <div>
+        Footer
+    </div>
+</template>
