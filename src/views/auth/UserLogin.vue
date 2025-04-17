73cce0af83372d377feedec3dca64ed48928bddd
VictorMRD <victormrd2002@hotmail.com>
2025-04-17 14:34:57 -0700

    Add change style button

--- src/views/auth/UserLogin.vue
+++ src/views/auth/UserLogin.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
     import { Button } from '@/components/ui/button'
+    import { Input } from '@/components/ui/input'
+
+    function login() {
+        console.log('Login button clicked')
+    }
+
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center bg-gray-400 w-50">
-        <p>Login</p>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <Button>Login</Button>
+    <div class="flex flex-col items-center justify-center w-50 gap-2">
+        <p class="font-bold text-2xl">Login page</p>
+        <Input type="text" placeholder="Username" />
+        <Input type="password" placeholder="Password" />
+        <Button variant="secondary" @click="login">Login</Button>
     </div>
 </template>
