f153eff723f2b50700dbb11c1b280c10dcf75f6f
VictorMRD <victormrd2002@hotmail.com>
2025-04-17 22:17:38 -0700

    Add navigation menu with RouterLinks

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
     import Button from '@/components/ui/button/Button.vue';
-    import { ref } from 'vue';
     import { RouterLink, RouterView } from 'vue-router'
     import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
     import { Icon } from '@iconify/vue'
@@ -19,47 +18,26 @@
 
     const components: { title: string, href: string, description: string }[] = [
       {
-        title: 'Alert Dialog',
-        href: '/docs/components/alert-dialog',
+        title: 'Material para clases',
+        href: '/teacher/materials',
         description:
-          'A modal dialog that interrupts the user with important content and expects a response.',
+          'Materiales puestos a la dispocisión pública del docente para su uso gratuito.',
       },
       {
-        title: 'Hover Card',
-        href: '/docs/components/hover-card',
+        title: 'Cursos gratuitos',
+        href: '/teacher/courses',
         description:
-          'For sighted users to preview content available behind a link.',
-      },
-      {
-        title: 'Progress',
-        href: '/docs/components/progress',
-        description:
-          'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-      },
-      {
-        title: 'Scroll-area',
-        href: '/docs/components/scroll-area',
-        description: 'Visually or semantically separates content.',
-      },
-      {
-        title: 'Tabs',
-        href: '/docs/components/tabs',
-        description:
-          'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-      },
-      {
-        title: 'Tooltip',
-        href: '/docs/components/tooltip',
-        description:
-          'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-      },
+          'Cursos gratuitos para el docente, con validez oficial.',
+      }
     ]
 </script>
 <template>
     <div class="flex justify-between items-center py-2 px-2 border-b-2 border">
         <div class="flex gap-2 items-center">
           <div>
-            <Button variant="ghost" class="text-xl font-bold select-auto">EDV</Button>
+            <RouterLink to="/" class="flex items-center">
+              <Button variant="ghost" class="text-xl font-bold select-auto">EDV</Button>
+            </RouterLink>
           </div>
           <div>
             <NavigationMenu>
@@ -70,10 +48,7 @@
                     <ul class="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[minmax(0,.75fr)_minmax(0,1fr)]">
                       <li class="row-span-3">
                         <NavigationMenuLink as-child>
-                          <a
-                            class="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                            href="/"
-                          >
+                            <Button class="hidden" disabled></Button>
                             <img src="../assets/images/SEP_logo.png" class="w-full">
                             <div class="mb-2 mt-4 text-lg font-medium">
                               Secretaria de Educación Pública
@@ -81,76 +56,61 @@
                             <p class="text-sm leading-tight text-muted-foreground">
                               Sección dedicada a la publicación digerida de la información publicada por la SEP
                             </p>
-                          </a>
                         </NavigationMenuLink>
                       </li>
 
                       <li>
                         <NavigationMenuLink as-child>
-                          <a
-                            href="/docs/introduction"
-                            class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                          >
-                            <div class="text-sm font-medium leading-none">Introduction</div>
+                          <RouterLink to="/sep/usicamm" class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground">
+                            <div class="text-sm font-medium leading-none">USICAMM</div>
                             <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                              Re-usable components built using Radix UI and Tailwind CSS.
+                              Información oficial del USICAMM.
                             </p>
-                          </a>
+                          </RouterLink>
                         </NavigationMenuLink>
                       </li>
                       <li>
                         <NavigationMenuLink as-child>
-                          <a
-                            href="/docs/installation"
+                          <RouterLink
+                            to="/sep/SepNews"
                             class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                           >
-                            <div class="text-sm font-medium leading-none">Installation</div>
+                            <div class="text-sm font-medium leading-none">Noticias</div>
                             <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                              How to install dependencies and structure your app.
+                              Declaraciones publicas y oficiales de la SEP.
                             </p>
-                          </a>
-                        </NavigationMenuLink>
-                      </li>
-                      <li>
-                        <NavigationMenuLink as-child>
-                          <a
-                            href="/docs/typography"
-                            class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-                          >
-                            <div class="text-sm font-medium leading-none">Typography</div>
-                            <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
-                              Styles for headings, paragraphs, lists...etc
-                            </p>
-                          </a>
+                          </RouterLink>
                         </NavigationMenuLink>
                       </li>
                     </ul>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                  <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                  <NavigationMenuTrigger>Docentes</NavigationMenuTrigger>
                   <NavigationMenuContent>
                     <ul class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
                       <li v-for="component in components" :key="component.title">
                         <NavigationMenuLink as-child>
-                          <a
-                            :href="component.href"
+                          <RouterLink
+                            :to="component.href"
                             class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
                           >
                             <div class="text-sm font-medium leading-none">{{ component.title }}</div>
                             <p class="line-clamp-2 text-sm leading-snug text-muted-foreground">
                               {{ component.description }}
                             </p>
-                          </a>
+                          </RouterLink>
                         </NavigationMenuLink>
                       </li>
                     </ul>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                  <NavigationMenuLink href="/docs/introduction" :class="navigationMenuTriggerStyle()">
-                    Documentation
-                  </NavigationMenuLink>
+                  <RouterLink to="/about">
+                    <NavigationMenuLink :class="navigationMenuTriggerStyle()">
+                      Acerca de mi
+                    </NavigationMenuLink>
+                  </RouterLink>
                 </NavigationMenuItem>
               </NavigationMenuList>
             </NavigationMenu>
